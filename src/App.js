318da4dd8836c0ce2f5d4178fc9a64393ae0eb93
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-09-15 06:13:29 +0500

    some fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/navbar/Navbar';
+import Projects from './components/projects/Projects';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Projects.module.css'
+
+const Projects = () => {
+  return (
+    <div className={styles.body}>
+      <div className={styles.body}>
+        <h1>
+        О проекте
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
