07f13e3f389466bb06ca653ac27e8d84f0712e82
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-01-17 21:36:44 +0500

    change the functional component to the arrow component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import Map from './components/map/Map';
-import City from './components/city/City';
-import WhyWe from './components/whywe/WhyWe';
-import Navbar from './components/navbar/Navbar';
-import Payment from './components/payment/Payment';
-import Projects from './components/projects/Projects';
-import Location from './components/location/Location';
-import Download from './components/download/Download';
-import Technology from './components/technology/Technology';
-import EndSection from './components/endSection/EndSection';
+import Map from './components/map/Map'
+import City from './components/city/City'
+import WhyWe from './components/whywe/WhyWe'
+import Navbar from './components/navbar/Navbar'
+import Payment from './components/payment/Payment'
+import Projects from './components/projects/Projects'
+import Location from './components/location/Location'
+import Download from './components/download/Download'
+import Technology from './components/technology/Technology'
+import EndSection from './components/endSection/EndSection'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Navbar/>
-      <Projects/>
-      <Location/>
-      <City/>
-      <WhyWe/>
-      <Technology/>
-      <Download/>
-      <Payment/>
-      <Map/>
-      <EndSection/>
+      <Navbar />
+      <Projects />
+      <Location />
+      <City />
+      <WhyWe />
+      <Technology />
+      <Download />
+      <Payment />
+      <Map />
+      <EndSection />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
